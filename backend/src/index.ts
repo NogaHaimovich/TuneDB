21b2fa6bef9c0a23008dd7c4733d7ff970e82af4
NogaHaimovich <noga.haimovich153@gmail.com>
2025-12-09 11:29:11 +0200

    add helmet for security

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,10 +2,13 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
-import deezerRoutes from "./routes/deezer.routes.js"
-import userRoutes from "./routes/user.routes.js"
-import playlistRoutes from "./routes/playlist.routes.js"
 import cors from "cors";
+import helmet from "helmet";
+
+import deezerRoutes from "./routes/deezer.routes.js";
+import userRoutes from "./routes/user.routes.js";
+import playlistRoutes from "./routes/playlist.routes.js";
+
 import connectDB from "./db/db.js";
 
 const app = express();
@@ -14,7 +17,7 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 const allowedOrigins = [
-  "http://localhost:5173", 
+  "http://localhost:5173",
   "http://localhost:5174",
   "https://tunedb.netlify.app"
 ];
@@ -23,15 +26,25 @@ if (process.env.FRONTEND_URL) {
   allowedOrigins.push(process.env.FRONTEND_URL);
 }
 
-app.use(cors({
-  origin: allowedOrigins,
-  credentials: true
-}));
+app.use(
+  helmet({
+    crossOriginEmbedderPolicy: false, 
+    contentSecurityPolicy: false,    
+  })
+);
 
 app.use(express.json());
-app.use("/api/deezer", deezerRoutes)
-app.use("/api/user", userRoutes)
-app.use("/api/playlist", playlistRoutes)
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
+
+app.use("/api/deezer", deezerRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/playlist", playlistRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
